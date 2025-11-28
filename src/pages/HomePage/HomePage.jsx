b925a606cc40c9e89370c3026d050e482c37596b
Jenna-Zing <47491259+Jenna-Zing@users.noreply.github.com>
2025-11-27 18:18:18 -0600

    basic Home page info - need to update styling

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,37 @@
+import styles from "./HomePage.module.css";
+
 export default function HomePage() {
   return (
     <>
-      <h1>Home Page</h1>
+      {/* TODO: add details about the app functionality */}
+      <div className={styles.container}>
+        <h1>Welcome Space Traveler!</h1>
+        <p>
+          <br />
+          Explore the wonders of the universe, from spacecrafts to distant
+          planets, and even build your own space station!
+        </p>
+        <div className={styles.features}>
+          <div className={styles.feature}>
+            <h3>Spacecrafts</h3>
+            <p>
+              Browse our collection of spacecrafts designed for interstellar
+              travel.
+            </p>
+            <ul>
+              <li>Construct your own spacecraft!</li>
+              <li>Destroy a spacecraft</li>
+            </ul>
+          </div>
+          <div className={styles.feature}>
+            <h3>Planets</h3>
+            <p>
+              Discover new and exciting planets waiting to be explored by
+              intrepid space travelers.
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
