f2266ae5726dfca8ec6bb079c850d50428eaad45
Jenna-Zing <47491259+Jenna-Zing@users.noreply.github.com>
2025-12-02 15:25:16 -0600

    meyerweb stylesheet from starter code causes styling issues...  had to manually add styling for headers and lists in HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,30 +5,34 @@ export default function HomePage() {
     <>
       {/* TODO: add details about the app functionality */}
       <div className={styles.container}>
-        <h1>Welcome Space Traveler!</h1>
-        <p>
-          <br />
-          Explore the wonders of the universe, from spacecrafts to distant
-          planets, and even build your own space station!
-        </p>
+        <header className={styles.header}>
+          <h1>Welcome Space Traveler!</h1>
+          <p>
+            Explore the wonders of the universe, from spacecrafts to distant
+            planets, and even build your own space station!
+          </p>
+        </header>
         <div className={styles.features}>
           <div className={styles.feature}>
             <h3>Spacecrafts</h3>
-            <p>
-              Browse our collection of spacecrafts designed for interstellar
-              travel.
-            </p>
             <ul>
+              <li>
+                Browse our collection of spacecrafts designed for interstellar
+                travel.
+              </li>
               <li>Construct your own spacecraft!</li>
               <li>Destroy a spacecraft</li>
             </ul>
           </div>
           <div className={styles.feature}>
             <h3>Planets</h3>
-            <p>
-              Discover new and exciting planets waiting to be explored by
-              intrepid space travelers.
-            </p>
+            <ul>
+              <li>
+                Discover new and exciting planets waiting to be explored by
+                intrepid space travelers.
+              </li>
+              <li>Click a planet then the desired spacecraft to move it!</li>
+            </ul>
           </div>
         </div>
       </div>
